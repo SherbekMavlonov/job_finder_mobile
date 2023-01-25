6eb7db4465cef24f51d49f5d54a8431ae1623d55
SHERBEK <mavlonovsherbek43@gmail.com>
2023-01-26 04:38:26 +0500

    Initial

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+export default () => {
+  return (
+    <SafeAreaView>
+      <Text>Test</Text>
+    </SafeAreaView>
+  );
+};
