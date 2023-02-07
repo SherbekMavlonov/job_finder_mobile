affd1feff1ced3f89d59ff8de5002cf2356e349d
SHERBEK <mavlonovsherbek43@gmail.com>
2023-02-07 12:56:28 +0500

    FIREBASE setup

--- ios/jobfinder_mobile/AppDelegate.mm
+++ ios/jobfinder_mobile/AppDelegate.mm
@@ -2,14 +2,18 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <Firebase.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"jobfinder_mobile";
+   [FIRApp configure];
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
